b66930af64cf3812f2ddd04188ddc4f260061840
Ibrahim Hanna <ibrahim.seniore@gmail.com>
2022-01-31 16:34:57 +0200

    margin

--- src/com/hello/Hello.java
+++ src/com/hello/Hello.java
@@ -3,6 +3,6 @@ package com.hello;
 public class Hello {
    public static void main(String [] args) {
 	   
-	  System.out.println("Jello Hello testing");
+	  System.out.println("Jello Hello Margin");
    }
 }

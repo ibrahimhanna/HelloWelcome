d1ebbd153655d2831c4f5a1f4f3016aebe5d5fe4
ibrahimhanna <44709558+ibrahimhanna@users.noreply.github.com>
2022-01-31 16:16:30 +0200

    Update Hello.java

--- src/com/hello/Hello.java
+++ src/com/hello/Hello.java
@@ -3,6 +3,6 @@ package com.hello;
 public class Hello {
    public static void main(String [] args) {
 	   
-	  System.out.println("Jello Hello");
+	  System.out.println("Jello Hello testing");
    }
 }
